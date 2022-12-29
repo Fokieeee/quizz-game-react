2e848839aeb187ec75d44fdecd2420848b9e171f
Fokieeee <114172954+Fokieeee@users.noreply.github.com>
2022-12-29 14:49:22 +0500

    changed name and title

--- src/Components/Start/Start.jsx
+++ src/Components/Start/Start.jsx
@@ -4,7 +4,7 @@ import cl from "./Start.module.css";
 export const Start = ({ setGameStart }) => {
   return (
     <div className={cl.start}>
-      <h1 className={cl.title}>Quizzical</h1>
+      <h1 className={cl.title}>Quizz Game</h1>
       <h3 className={cl.description}>Answer some questions and have fun!!!</h3>
       <button onClick={() => setGameStart(false)} className={cl.btn}>
         Start Game
