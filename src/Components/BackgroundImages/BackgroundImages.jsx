34e93d4bccafda9707f8463ac25f8d7d82fff69d
Fokieeee <114172954+Fokieeee@users.noreply.github.com>
2023-01-24 21:27:05 +0500

    dfsf

--- src/Components/BackgroundImages/BackgroundImages.jsx
+++ src/Components/BackgroundImages/BackgroundImages.jsx
@@ -4,14 +4,8 @@ import cl from "./BackgroundImages.module.css";
 export const BackgroundImages = () => {
   return (
     <div>
-      <img
-        className={cl.left}
-        src="./src/Components/BackgroundImages/images/blob.svg"
-      />
-      <img
-        className={cl.right}
-        src="./src/Components/BackgroundImages//images/blob1.svg"
-      />
+      <img className={cl.left} src="./images/blob.svg" />
+      <img className={cl.right} src="./images/blob1.svg" />
     </div>
   );
 };
