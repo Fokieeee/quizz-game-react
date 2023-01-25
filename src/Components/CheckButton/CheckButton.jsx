c8d805e4d5840f5e7b315ced8ca0c28bc94867dc
Fokieeee <114172954+Fokieeee@users.noreply.github.com>
2023-01-25 14:20:48 +0500

    changed url of BackgroundImages

--- src/Components/CheckButton/CheckButton.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import cl from './CheckButton.module.css'
-
-export const CheckButton = ({checkResult}) => {
-  return (
-    <button onClick={() => checkResult()} className={cl.btn}>Check Answers</button>
-  )
-}
